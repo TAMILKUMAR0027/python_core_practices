1e0339b4b79ce550fa5540b3dc56baaa23aa2b1d
TAMILKUMAR0027 <tamilkumar0027@gmail.com>
2026-05-21 17:15:37 +0530

    Python pratice

---
+++ Boolean_Literal.py
@@ -0,0 +1,11 @@
+a=1
+
+print(type(a) is int)
+
+b=a
+print(a is b)
+print(a,b)
+print(a is not b)
+
+a=[1,2,3]
+print(10 not in a)
---
+++ typeConversion.py
@@ -0,0 +1,9 @@
+num1 = 10
+print("Odw","adsq",sep=",",end=".\n")
+num2 = int(input("Enter a number: "))
+str=input("")
+print(type(num1 + num2))
+print(type(float(num1 + num2)))
+print(type(num2))
+print(type(num2 + 90.2))
+print(str)
